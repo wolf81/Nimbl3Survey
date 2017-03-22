653560fc90d519cfdef9a06fc85a52b51901b3bf
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2017-03-22 13:25:32 +0700

    Setup project & cocoapods.

---
+++ Nimbl3SurveyTests/Nimbl3SurveyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Nimbl3SurveyTests.swift
+//  Nimbl3SurveyTests
+//
+//  Created by Wolfgang Schreurs on 22/03/2017.
+//  Copyright © 2017 Wolftrail. All rights reserved.
+//
+
+import XCTest
+@testable import Nimbl3Survey
+
+class Nimbl3SurveyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+
+use_frameworks!
+
+target 'Nimbl3Survey' do
+
+end
